0212fcbdb4748581654b87a4cd8715948eee562e
Orazalinov Askhat <58249226+askesaske@users.noreply.github.com>
2021-04-03 02:36:06 +0600

    header and footer finished

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,89 @@
 <template>
-  <div>
-    footer
-  </div>
+  <footer class="footer">
+    <div class="footer__container">
+
+      <div class="footer__main">
+
+        <div class="footer__contacts">
+
+          <div class="footer__row footer__row--start">
+            <svg class="footer__icon" width="20" height="20">
+              <use href="../assets/img/icons.svg#phone"></use>
+            </svg>
+
+            <div class="footer__phone-box">
+              <div class="footer__phone">
+                <a href="tel: +7 (778) 200 20 50" class="footer__text">
+                  +7 (778) 200 20 50
+                </a>
+
+                <span class="footer__light-text">Аренда залов</span>
+              </div>
+
+              <div class="footer__phone">
+                <a href="tel: +7 (777) 078 87 78" class="footer__text">
+                  +7 (777) 078 87 78
+                </a>
+
+                <span class="footer__light-text">Аренда рабочего места</span>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="footer__row">
+            <svg class="footer__icon" width="20" height="20">
+              <use href="../assets/img/icons.svg#mail"></use>
+            </svg>
+
+            <p class="footer__text">hello@smartpoint.me</p>
+          </div>
+
+          <div class="footer__row">
+            <svg class="footer__icon" width="20" height="20">
+              <use href="../assets/img/icons.svg#map-pin"></use>
+            </svg>
+
+            <p class="footer__text">г. Алматы, ул. Байзакова, 280</p>
+          </div>
+
+          <div class="footer__socials">
+            <a href="#" class="footer__social"><img src="../assets/img/icons/insta.png" alt=""></a>
+            <a href="#" class="footer__social"><img src="../assets/img/icons/facebook.png" alt=""></a>
+            <a href="#" class="footer__social"><img src="../assets/img/icons/telegram.png" alt=""></a>
+          </div>
+
+        </div>
+
+        <div class="footer__links">
+          <a href="#" class="footer__text">О нас</a>
+          <a href="#" class="footer__text">Наша команда</a>
+          <a href="#" class="footer__text">Комьюнити</a>
+          <a href="#" class="footer__text">Наша галерея</a>
+          <a href="#" class="footer__text">Пространство</a>
+          <a href="#" class="footer__text">Онлайн бронирование</a>
+          <a href="#" class="footer__text">Тарифы</a>
+          <a href="#" class="footer__text">Мероприятия</a>
+          <a href="#" class="footer__text">Новости</a>
+        </div>
+
+        <div class="footer__pattern">
+          <img src="../assets/img/footer-logo.png" alt="" class="footer__logo">
+          <p class="footer__text">
+            пространство разумных технологий <br>
+            и креативных решений <br>
+            <span>smartpoint.me</span>
+          </p>
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="footer__copyrights">
+      All rights reserved © 2021 smartpoint.me
+    </div>
+  </footer>
 </template>
 
 <script>
--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>
-    HEADER
-  </div>
+  <header class="header">
+    <div class="header__container">
+      <nuxt-link tag="a" to="/" class="header__logo">
+        <img src="../assets/img/logo.png" alt="">
+      </nuxt-link>
+
+      <div class="header__items">
+        <div class="header__location">
+          <svg width="20" height="20">
+            <use href="../assets/img/icons.svg#map-pin"></use>
+          </svg>
+
+          <p>Алматы</p>
+        </div>
+
+        <div class="header__lang-box">
+          РУС
+        </div>
+
+        <div class="header__user-box">
+          <svg width="20" height="20">
+            <use href="../assets/img/icons.svg#user"></use>
+          </svg>
+        </div>
+
+        <svg class="header__burger" width="20" height="20">
+          <use href="../assets/img/icons.svg#burger"></use>
+        </svg>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
